0019faad689421da08604306753b70b43050f878
kdhsea <kdhsea@gmail.com>
2020-12-24 03:06:51 +0900

    portfolio_add_tab_function portfolio_revise_nav_function portfolio_revise_color_function

--- js/color.js
+++ js/color.js
@@ -49,32 +49,39 @@ var btn ={
     document.querySelector('#login_btn').style.color = color;
   },
 
+  LogInBorderSetColor : function(color){
+    document.querySelector('#login_btn').style.borderColor = color;
+  }
+
+
 }
 
 function nightdayhandler(self){
 
     var target = document.querySelector('body');
-    var target1 = document.querySelector('#night_day_btn')
-    var target2 = document.querySelector('#login_btn')
-    var target3 = document.querySelector('#search_area')
+    var target1 = document.querySelector('#night_day_btn');
+    var target2 = document.querySelector('#login_btn');
+    var target3 = document.querySelector('#search_area');
+    var target4 = document.querySelector('nav');
+    var target5 = document.querySelector('#main_title-2');
 
     if(self.value === 'night'){
 // nigth 모드 일때
-      Body.SetBackgroundColor('#282C34');
+      Body.SetBackgroundColor('#0D1117');
       Body.SetColor('white');
 
       btn.NightDaySetBackgroundColor('white');
-      btn.LoginSetBackgroundColor('F7E444');
+      btn.LoginSetBackgroundColor('#004680');
       btn.NightDaySetColor('#282C34');
-      btn.LogInSetColor('#282C34');
+      btn.LogInSetColor('white');
+      btn.LogInBorderSetColor('#004680');
 
-
-      target3.style.backgroundColor  = '#282C34';
-      target3.style.color  = 'white';
+      target3.style.backgroundColor = "black";
+      target4.style.backgroundColor = "#161B22";
 
       self.value = 'day';
 
-      Links.SetColor('powderblue','white')
+      Links.SetColor('#F6F0FC')
 
       }
 
@@ -85,17 +92,21 @@ function nightdayhandler(self){
       Body.SetBackgroundColor('#F4F4F4');
 
       btn.NightDaySetBackgroundColor('#282C34');
-      btn.LoginSetBackgroundColor('F7E444');
+      btn.LoginSetBackgroundColor('#6587C6');
       btn.NightDaySetColor('white');
       btn.LogInSetColor('white');
-
+      btn.LogInBorderSetColor('#6587C6');
 
       target3.style.backgroundColor  = 'white';
-      target3.style.color  = '#dadada';
+      target4.style.backgroundColor = "#f4f4f4";
+      target5.style.color = "#6587C6";
+
+
 
       self.value = 'night';
 
-      Links.SetColor('#939597','#F7E444')
+      Links.SetColor('#939597')
+      target5.style.color = "#6587C6";
 
     }
 
--- js/navigation.js
+++ js/navigation.js
@@ -1,14 +1,1 @@
 window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.querySelector("nav").style.padding =  "5px 0";
-      document.querySelector("nav").style.backgroundColor = "black";
-
-
-  } else {
-    document.querySelector("nav").style.padding = "5px 0";
-    document.querySelector("nav").style.backgroundColor = "white";
-
-  }
-}
