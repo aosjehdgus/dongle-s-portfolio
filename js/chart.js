f752f14e48dd29562f4527fe3c1fe3cbd85f920b
kdhsea <kdhsea@gmail.com>
2021-01-31 19:30:28 +0900

    Add skill proficiency chart

---
+++ js/chart.js
@@ -0,0 +1,36 @@
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['HTML', 'CSS', 'Javascript', 'React', 'Redux'],
+    datasets: [{
+      label: '숙련도',
+      data: [9, 10, 8, 7, 6],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(97, 218, 251, 0.2)',
+        'rgba(118, 74, 188, 0.2)',
+        
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(97, 218, 251, 1)',
+        'rgba(118, 74, 188, 1)',
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+}); 
