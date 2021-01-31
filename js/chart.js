8d3e0012627028194f2c72e8b91b62e6807975ca
kdhsea <kdhsea@gmail.com>
2021-01-31 20:56:22 +0900

    Fix data in chart.js

--- js/chart.js
+++ js/chart.js
@@ -1,11 +1,11 @@
 var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
-  type: 'bar',
+  type: 'horizontalBar',
   data: {
     labels: ['HTML', 'CSS', 'Javascript', 'React', 'Redux'],
     datasets: [{
       label: '숙련도',
-      data: [9, 10, 8, 7, 6],
+      data: [92, 97, 83, 75, 64],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -21,12 +21,13 @@ var myChart = new Chart(ctx, {
         'rgba(97, 218, 251, 1)',
         'rgba(118, 74, 188, 1)',
       ],
-      borderWidth: 1
+      borderWidth: 0
     }]
   },
   options: {
+    responsive: false,
     scales: {
-      yAxes: [{
+      xAxes: [{
         ticks: {
           beginAtZero: true
         }
