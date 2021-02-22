6e7c4282bff6450788e6380309b32fc7824c8d45
kdhsea <kdhsea@gmail.com>
2021-02-23 03:13:18 +0900

    Revise skill section Change the graph and add skill related images and descriptions

--- js/chart.js
+++ js/chart.js
@@ -1,37 +1,26 @@
-var ctx = document.getElementById('myChart');
-var myChart = new Chart(ctx, {
-  type: 'horizontalBar',
-  data: {
-    labels: ['HTML', 'CSS', 'Javascript', 'React', 'Redux'],
-    datasets: [{
-      label: '숙련도',
-      data: [92, 97, 83, 75, 64],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(97, 218, 251, 0.2)',
-        'rgba(118, 74, 188, 0.2)',
-        
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(97, 218, 251, 1)',
-        'rgba(118, 74, 188, 1)',
-      ],
-      borderWidth: 0
-    }]
-  },
-  options: {
-    responsive: false,
-    scales: {
-      xAxes: [{
-        ticks: {
-          beginAtZero: true
+setTimeout(function start (){
+
+  
+  
+  $('.bar').each(function(i){  
+    var $bar = $(this);
+    $(this).append('<span class="count"></span>');
+    setTimeout(function(){
+      
+      $bar.css('width', $bar.attr('data-percent'));      
+    }, i*100);
+  });
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).parent('.bar').attr('data-percent')
+    }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now) +'%');
         }
-      }]
-    }
-  }
-}); 
+    });
+});
+
+}, 1000)
