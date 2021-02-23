1abdc97f0adccf3f221990ba096b1383909e1c79
kdhsea <kdhsea@gmail.com>
2021-02-23 17:00:37 +0900

    Remove dom.js

--- js/dom.js
+++
@@ -1,5 +0,0 @@
-var newParagraph = document.createElement("p");
-var paragraphContent = document.createTextNode("I'm new!");
-
-newParagraph.appendChild(paragraphContent);
-document.body.appendChild(newParagraph);
