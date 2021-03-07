0dd2a822e11d7ac12c44ba106a63a8796cf20653
kdhsea <kdhsea@gmail.com>
2021-03-07 21:50:36 +0900

    Add nodejs server first version

---
+++ main.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+ 
+});
+app.listen(3000);
+
+// 사용자에게 전송할 데이터를 생성한다 (nodejs가 가지고 있는 힘)
