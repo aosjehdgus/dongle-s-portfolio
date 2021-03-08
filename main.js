d5523fdd31e8437063c5760c7f523c84c990064d
kdhsea <kdhsea@gmail.com>
2021-03-08 13:09:44 +0900

    Apply dynamic web pages

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
 var http = require('http');
 var fs = require('fs');
+// url이라는 모듈을 url이라는 변수로 사용할 것이다.
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    if(_url == '/'){
+      title = 'Welcome';
     }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
+    if(_url == '/favicon.ico'){
+      return response.writeHead(404);
+       
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    
+    var template = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${title}</title>
+    </head>
+    <body>
+        <h1><a href = "/">WEB</a></h1>
+        <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=Javascript">Javascript</a></li>
+        </ul>
+
+        <h2>${title}</h2>
+
+        <p>HTML is HTML</p>
+        
+    </body>
+    </html>
+    
+    `;
+
+    response.end(template);
+    
  
 });
 app.listen(3000);
